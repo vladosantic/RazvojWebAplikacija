7c8bca790052c6a27fba7d8d844eb34f1f473785
Vlado <vlado.santic@fsre.sum.ba>
2022-02-05 19:53:26 +0100

    sortiranje po cijeni

---
+++ public/js/skripta.js
@@ -0,0 +1,40 @@
+function funkcija(value){
+	if(value == "jeftino"){
+		var sortiranje = document.getElementById('proizvodi').children;
+		sortiranje = Array.prototype.slice.call(sortiranje, 0);
+		var pom;
+		for (var i = 0; i < sortiranje.length - 1; i++){
+		for(var j = 0; j < sortiranje.length - 1 - i; j++){
+			if(sortiranje[j].querySelector('h4').innerHTML.slice(0,2) > sortiranje[j+1].querySelector('h4').innerHTML.slice(0,2)){
+				pom = sortiranje[j];
+				sortiranje[j] = sortiranje[j+1];
+				sortiranje[j+1] = pom;
+			} 
+		}
+	}
+		var parent = document.getElementById('proizvodi');
+		parent.innerHTML = " ";
+		for(i = 0; i < sortiranje.length; i++){
+			parent.appendChild(sortiranje[i]);
+		}
+	}
+	else if(value == "skupo"){
+		var sortiranje = document.getElementById('proizvodi').children;
+		sortiranje = Array.prototype.slice.call(sortiranje, 0);
+		var pom;
+		for (var i = 0; i < sortiranje.length - 1; i++){
+		for(var j = 0; j < sortiranje.length - 1 - i; j++){
+			if(sortiranje[j].querySelector('h4').innerHTML.slice(0,2) < sortiranje[j+1].querySelector('h4').innerHTML.slice(0,2)){
+				pom = sortiranje[j];
+				sortiranje[j] = sortiranje[j+1];
+				sortiranje[j+1] = pom;
+			} 
+		}
+	}
+		var parent = document.getElementById('proizvodi');
+		parent.innerHTML = " ";
+		for(i = 0; i < sortiranje.length; i++){
+			parent.appendChild(sortiranje[i]);
+		}
+	}
+}
--- resources/views/proizvodi.blade.php
+++ resources/views/proizvodi.blade.php
@@ -6,6 +6,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
         <meta charset="UTF-8">
         <title>Online ljekarna Sanitas</title>
+        <script src="js/skripta.js"></script>
         <link rel="icon" type="image/x-icon" href="{{ URL::asset('assets/santass.jpg') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/projekt.css') }}">
         <style>
@@ -35,12 +36,12 @@
     </div>
     </div></br>
     <div id="proizvodbar">
-        <label for="filter">Filtriranje</label>
-        <select name="filter" id="opcija">
-            <option value="auzlazno">Abecedno (uzlazno)</option>
-            <option value="asilazno">Abecedno (silazno)</option>
-            <option value="jeftino" selected>Po cijeni (od najjeftinijeg)</option>
-            <option value="skupo">Po cijeni (od najskupljeg)</option>
+        <label>Filtriranje</label>
+        <select id="izbor" onchange="funkcija(this.value)">
+            <option value="abecednou">Abecedno(uzlazno)</option>
+            <option value="abecednos">Abecedno(silazno)</option>
+            <option value="jeftino">Po cijeni(najjeftinije prvo)</option>
+            <option value="skupo">Po cijeni(najskuplje prvo)</option>
         </select>
     </div>
     <div id="proizvodi">
@@ -48,56 +49,56 @@
         <img class="slikapr" src="assets/aspirin.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Aspirin tablete</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 20,70 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>20,70 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="24" height="20"></div>
     </div>
     <div class="proizvod">
         <img class="slikapr" src="assets/alpenkraft.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Alpenkraft sirup</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 26,50 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>26,50 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="24" height="20"></div>
     </div>
     <div class="proizvod">
         <img class="slikapr" src="assets/biotic.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Omni Biotic Metabolic vrećice</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 88,00 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>88,00 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="24" height="20"></div>
     </div>
     <div class="proizvod">
         <img class="slikapr" src="assets/antemig.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Pileje Antemig tablete</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 59,90 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>59,90 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="20" height="20"></div>
     </div>
     <div class="proizvod">
         <img class="slikapr" src="assets/osteopan.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Osteopan Plus tablete (60 tableta)</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 25,00 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>25,00 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="20" height="20"></div>
     </div>
     <div class="proizvod">
         <img class="slikapr" src="assets/yasenka.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Yasenka Imuno BC 500 kapsule</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 53,50 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>53,50 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="20" height="20"></div>
     </div>
     <div class="proizvod">
         <img class="slikapr" src="assets/pip_farmakol.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Pip Farmakol Propolis 150 sprej</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 14,40 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>14,40 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="20" height="20"></div>
     </div>
     <div class="proizvod">
         <img class="slikapr" src="assets/arkovital.png" alt="Slika proizvoda" width="256px" height="256px">
         <div class="ime_proizvoda">Arkopharma Arkovital Pure Energy tablete</div>
         <br/>
-        <div class="cijena_proizvoda">Cijena: 37,20 KM</div>
+        <p class="cijena_proizvoda">Cijena:</p><h4>37,20 KM</h4>
         <div class="kosarica">U košaricu<img src="assets/cart.png" width="20" height="20"></div>
     </div>
 </div>
